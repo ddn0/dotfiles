d5639b139eee261de7ba4b2b74171d0e71afd4b4
Donald Nguyen <ddn0@users.noreply.github.com>
2020-07-25 20:57:44 +0000

    Idempotent installs

--- install-extra.sh
+++ install-extra.sh
@@ -1,2 +1,10 @@
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#!/bin/bash
+if [ ! -e ~/.fzf ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
+
+if [ ! -e ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
